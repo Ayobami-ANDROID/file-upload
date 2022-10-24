0b98ed7f94f8a22295211da29fa5b5dbb5dc8d26
Ayobami Ajetunmobi <ajetunmobiayobami19@gmail.com>
2022-10-24 21:21:57 +0100

    my messages

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,63 @@ const { UploadStream } = require('../utils/cloudinary')
 const router = express.Router()
 const cloudinary = require('../utils/cloudinary')
 const upload = require('../utils/multer')
+const User = require('../models/user')
+const { StatusCodes } = require('http-status-codes')
+// const statusCodes = require('http-status-codes')
 
 router.post('/', upload.single('image') ,async (req,res)=>{
     try {
         const result = await cloudinary.uploader.upload(req.file.path)
-        res.json(result)
+        let users ={
+            name:req.body.name,
+            avatar:result.secure_url,
+            cloudinary_id:result.public_id
+        }
+
+        const user = await User.create(users) 
+        res.status(StatusCodes.CREATED).json({user})
+
+    
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/',async(req,res)=>{
+    try {
+        const user = await User.find({})
+        res.status(StatusCodes.OK).json({user})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+    try {
+        let user = await User.findById({_id:req.params.id})
+        res.status(StatusCodes.OK).json({user})
+        await cloudinary.uploader.destroy(user.cloudinary_id) 
+        await user.remove()
+        res.status(StatusCodes).json(user)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.put('/:id',upload.single('image'),async(req,res)=>{
+    try {
+        let user = await User.findById({_id:req.params.id})
+        await cloudinary.uploader.destroy(user.cloudinary_id)
+        const result = cloudinary.uploader.upload(req.file.path)
+        const data ={
+            name: req.body.name || user.name,  
+            avatar:  result.secure_url || user.secure_url,
+            cloudinary_id:result.public_id || user.public_id
+        }
+        user = await User.findByIdAndUpdate({_id:req.params.id},data,{
+            new:true,runValidators:true
+        })
+        res.status(StatusCodes.OK).json({user})
     } catch (error) {
         console.log(error)
     }
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -6,9 +6,7 @@ cloudinary.config({
     api_key:process.env.cloudinary_cloud_api_key ,
     api_secret:process.env.cloudinary_cloud_api_secret,
   
-//     cloud_name: 'dftqjbuhr', 
-//   api_key: '767948195856298', 
-//   api_secret: 'dTzxwhwOfR7FL1ajWw60GXw8BTI'
+
 
 })
 
